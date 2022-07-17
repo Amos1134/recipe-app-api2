0a3500b8971797244d0ac9266aa551783cadfeee
Amos1134 <aamosaadesuyi@gmail.com>
2022-07-17 23:11:24 +0100

    Configured docker compose and checks to wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,10 +4,10 @@ Calculator function
 
 
 def add(x, y):
-  """Add x and y and retuen result."""
-  return x + y
+    """Add x and y and retuen result."""
+    return x + y
 
 
 def subtract(x, y):
-  """Subtract x from y and return result."""
-  return y - x
+    """Subtract x from y and return result."""
+    return y - x
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -21,6 +21,6 @@ class Command(BaseCommand):
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
-                self.stdout.write('Database unvailable, waiting for 1 second...')
+                self.stdout.write('DB unvailable, waiting for 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
